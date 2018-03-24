b9709f58c05a897a7f25e27345df08be94cd3d81
Franklin Yu <franklinyu@hotmail.com>
2018-03-24 11:11:37 -0400

    Rename '$key' to '$keys'
    Makes more sense as an array of keys.

--- manual/typora/tools/chocolateyUninstall.ps1
+++ manual/typora/tools/chocolateyUninstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName 'typora*'
+[array]$keys = Get-UninstallRegistryKey -SoftwareName 'typora*'
 
-if ($key.Count -eq 1) {
-  $key | % {
+if ($keys.Count -eq 1) {
+  $keys | % {
     $file = "$($_.UninstallString)"
 
     Uninstall-ChocolateyPackage -PackageName $env:ChocolateyPackageName `
@@ -12,12 +12,12 @@ if ($key.Count -eq 1) {
                                 -ValidExitCodes @(0) `
                                 -File "$file"
   }
-} elseif ($key.Count -eq 0) {
+} elseif ($keys.Count -eq 0) {
   Write-Warning "$env:ChocolateyPackageTitle has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
+} elseif ($keys.Count -gt 1) {
+  Write-Warning "$keys.Count matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
+  $keys | % {Write-Warning "- $_.DisplayName"}
   throw "Uninstallation failed"
 }
