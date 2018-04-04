49fbcede4a4129d0b3a14aca3b0ecc784be3bf0d
Franklin Yu <franklinyu@hotmail.com>
2018-04-04 00:50:10 -0400

    Upgrade Typora to 0.9.48
    Switch to version-specific download URL.

--- manual/typora/tools/chocolateyInstall.ps1
+++ manual/typora/tools/chocolateyInstall.ps1
@@ -2,12 +2,12 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://typora.io/windows/typora-setup-ia32.exe'
-  url64bit       = 'https://typora.io/windows/typora-setup-x64.exe'
+  url            = 'https://typora.io/windows/typora-update-ia32-0403.exe'
+  url64bit       = 'https://typora.io/windows/typora-update-x64-0403.exe'
 
-  checksum       = '3625f2c966f6a3ee9cc55473c3467188b2812e95ab0ae236bc71c3d3606a5770'
+  checksum       = '4a5ae49db458b7ab0d4992ab61d78464dd5c1918288d965ea408b3227c713843'
   checksumType   = 'sha256'
-  checksum64     = '4b7b15aa2219a3a60d9f17a5018165dcf9ca6079367ef3fa006d9e700d7a2f8d'
+  checksum64     = '861d24f81fc0557f06c4c2f28af8c2f324179dfbb068712265f15da26e26e653'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
