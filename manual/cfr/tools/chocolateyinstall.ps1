012bb3a39c8212dda7b27dde237450e3c27c57bf
Franklin Yu <franklinyu@hotmail.com>
2018-05-17 22:21:28 -0400

    Update CFR

--- manual/cfr/tools/chocolateyinstall.ps1
+++ manual/cfr/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     fileFullPath  = $jarPath
     url           = "http://www.benf.org/other/cfr/cfr_$($env:ChocolateyPackageVersion.Replace('.', '_')).jar"
-    checksum      = '3d6adb020c842cc8cb4198e7b38f256363987ba86fdb0272c1a7dcd08c0bbd26'
+    checksum      = '4A68B0FE1A4E1EB0061EED4387C3F00FE406C03325AF54E68F803204EE4B7B26'
     checksumType  = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
