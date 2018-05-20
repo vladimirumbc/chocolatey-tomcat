605413a97058eb968a0c36ba754fbccec5847752
Franklin Yu <franklinyu@hotmail.com>
2018-05-20 01:59:05 -0400

    Remove unrelated packages

--- bmfont/src/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'bmfont'
-$installerType = 'exe' 
-$url = 'http://www.angelcode.com/products/bmfont/install_bmfont_1.13.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
--- potplayer/src/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'potplayer';
-$installerType = 'exe';
-$url = 'http://get.daum.net/PotPlayer/v3/PotPlayerSetup.exe';
-$url64 = 'http://get.daum.net/PotPlayer64/v3/PotPlayerSetup64.exe';
-
-$silentArgs = '/S';
-$validExitCodes = @(0);
-
-Install-ChocolateyPackage -packageName $packageName -fileType $installerType -silentArgs $silentArgs -url $url -url64bit $url64  -validExitCodes $validExitCodes;
