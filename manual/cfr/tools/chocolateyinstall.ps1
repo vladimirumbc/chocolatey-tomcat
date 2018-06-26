28601deb8d4c36b0bb3703a9a6325462c50767e2
Franklin Yu <franklinyu@hotmail.com>
2018-06-26 15:51:04 -0400

    Fix bug in PowerShell 2
    `Get-Command` in PowerShell 2 is equivalent to `Get-Command -All` in
    PowerShell 3. See Example 10 in [this link][1] for details.
    
    [1]: http://adamringenberg.com/powershell2/get-command/

--- manual/cfr/tools/chocolateyinstall.ps1
+++ manual/cfr/tools/chocolateyinstall.ps1
@@ -1,19 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $jarPath = Join-Path -Path $toolsDir -ChildPath 'cfr.jar'
+$versionStr = $env:ChocolateyPackageVersion -replace '^(\d+)\.(\d+).*','$1_$2'
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     fileFullPath  = $jarPath
-    url           = "http://www.benf.org/other/cfr/cfr_$($env:ChocolateyPackageVersion.Replace('.', '_')).jar"
+    url           = "http://www.benf.org/other/cfr/cfr_$versionStr.jar"
     checksum      = 'b66a1e59891a2585885b171b4ec6fcc0e20f0d419e90a153e5c01540bb1cc67f'
     checksumType  = 'sha256'
 }
 Get-ChocolateyWebFile @packageArgs
 
-$javaCmd = Get-Command -Name java
-if ($javaCmd -eq $null -or $javaCmd.CommandType -ne 'Application')
+[array]$javaCmds = Get-Command -Name java
+if ($javaCmds[0] -eq $null -or $javaCmds[0].CommandType -ne 'Application')
 {
     throw 'Unexpected Java location'
 }
-Install-Binfile -Name $env:ChocolateyPackageName -Path $javaCmd.Definition -Command "`"-jar $jarPath`""
+Install-Binfile -Name $env:ChocolateyPackageName -Path $javaCmds[0].Definition -Command "`"-jar $jarPath`""
