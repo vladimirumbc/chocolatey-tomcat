d602991235b30c892280d82e673799ca40ae1640
Franklin Yu <franklinyu@hotmail.com>
2018-09-22 00:11:30 -0400

    Automate CFR
    Note that it's semi-automation. Continuous Integration isn't set up.

--- automatic/cfr/tools/chocolateyInstall.ps1
+++ automatic/cfr/tools/chocolateyInstall.ps1
@@ -1,12 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $jarPath = Join-Path -Path $toolsDir -ChildPath 'cfr.jar'
-$versionStr = $env:ChocolateyPackageVersion -replace '^(\d+)\.(\d+).*','$1_$2'
 
 $packageArgs = @{
     packageName   = $env:ChocolateyPackageName
     fileFullPath  = $jarPath
-    url           = "http://www.benf.org/other/cfr/cfr_$versionStr.jar"
+    url           = ''
     checksum      = '6e9bba18563f1b18396cde4fcd0bd4a18fcd930c92c44cb665cbc49528c12a90'
     checksumType  = 'sha256'
 }
---
+++ automatic/cfr/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$root = 'http://www.benf.org/other/cfr/'
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $root
+    [array]$href = $page.Links | Select-Object -ExpandProperty href | Select-String -Pattern '^cfr_(\d+)_(\d+).jar'
+    $groups = $href[0].Matches.Groups
+    $version = '{0}.{1}' -f $groups[1], $groups[2]
+    @{ Version = $version; Url = $root + $href[0] }
+}
+
+function global:au_SearchReplace {
+    @{
+        'cfr.nuspec' = @{
+            '<version>[.\d]+</version>' = '<version>{0}</version>' -f $Latest.Version
+        }
+        'tools\chocolateyInstall.ps1' = @{
+            '(^\s*url\s*)=.*' = "`$1= '{0}'" -f $Latest.Url
+            '(^\s*checksum\s*)=.*' = "`$1= '{0}'" -f $Latest.Checksum64
+        }
+    }
+}
+
+Update-Package
