469bf4998d7f273a945b664548ae383bd57f7187
Franklin Yu <franklinyu@hotmail.com>
2018-09-25 21:55:59 -0400

    Update Typora

--- manual/typora/tools/chocolateyInstall.ps1
+++ manual/typora/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$date = '0831'
+$date = '0912'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url            = "https://typora.io/windows/typora-update-ia32-$date.exe"
   url64bit       = "https://typora.io/windows/typora-update-x64-$date.exe"
 
-  checksum       = '1530fb4c57416b2685ccb814f3d779d20765a6df2a6bcaad1825508375290e2c'
+  checksum       = '7b78bcff921e3706af69fcca05b71f71f6dd3eaab95c7d82e89f35a6fc1bb9ef'
   checksumType   = 'sha256'
-  checksum64     = '962457f5177389b1a0a4fa09d25cd65102df13f504abbd6937b621e8295ef5a6'
+  checksum64     = '6a9126d820ac3840bc57340e346b0e0f30e5d5a8324b60d3cf1207872f0704d6'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
