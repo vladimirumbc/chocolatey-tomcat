56488e765691a720bb1378030f975fa294b3a411
Franklin Yu <franklinyu@hotmail.com>
2018-10-27 20:40:53 -0400

    Customize update script

--- update_all.ps1
+++ update_all.ps1
@@ -1,23 +1,19 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 
-param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\automatic")
+param([string] $Name, [string[]] $ForcedPackages)
 
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
-    Timeout       = 100                                     #Connection timeout in seconds
-    UpdateTimeout = 1200                                    #Update timeout in seconds
-    Threads       = 10                                      #Number of background jobs to use
-    Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
-    PluginPath    = ''                                      #Path to user plugins
+    Push = $Env:au_Push -eq 'true' #Push to chocolatey
 
     Report = @{
         Type = 'markdown'                                   #Report type: markdown or text
-        Path = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
-        Params= @{                                          #Report parameters:
+        Path = "$PSScriptRoot\Update-Report.md"         #Path where to save the report
+        Params = @{                                         #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = "[History](#update-history)"       #  Markdown, Text: Custom user message to show
+            UserMessage = "[History](#update-history)"      #  Markdown, Text: Custom user message to show
             NoIcons     = $false                            #  Markdown: don't show icon
             IconSize    = 32                                #  Markdown: icon size
             Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
@@ -30,16 +26,16 @@ $Options = [ordered]@{
         Path = "$PSScriptRoot\Update-History.md"            #Path where to save history
     }
 
-    Gist = @{
-        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
-    }
+    # Gist = @{
+    #     Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+    #     ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    #     Path   = "$PSScriptRoot\Update-Report.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+    # }
 
-    Git = @{
-        User     = ''                                       #Git username, leave empty if github api key is used
-        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-    }
+    # Git = @{
+    #     User     = ''                                       #Git username, leave empty if github api key is used
+    #     Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+    # }
 
     RunInfo = @{
         Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
@@ -60,7 +56,7 @@ $Options = [ordered]@{
              }
            } else {}
 
-    ForcedPackages = $ForcedPackages -split ' '
+    ForcedPackages = $ForcedPackages
     BeforeEach = {
         param($PackageName, $Options )
         $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
@@ -72,8 +68,8 @@ $Options = [ordered]@{
 }
 
 if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
-$global:au_Root = $Root                                    #Path to the AU packages
-$global:info = updateall -Name $Name -Options $Options
+$global:au_Root = 'automatic'
+$global:info = Update-AUPackages -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
-#if ($global:info.error_count.total) { throw 'Errors during update' }
+if ($global:info.error_count.total) { throw 'Errors during update' }
