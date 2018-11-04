d881d36dcd7b56214b1ef5ab5a46355012d2b188
Franklin Yu <franklinyu@hotmail.com>
2018-11-04 18:19:55 -0500

    Embed binary in CFR

--- automatic/cfr/tools/chocolateyInstall.ps1
+++ automatic/cfr/tools/chocolateyInstall.ps1
@@ -2,18 +2,8 @@ $ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $jarPath = Join-Path -Path $toolsDir -ChildPath 'cfr.jar'
 
-$packageArgs = @{
-    packageName   = $env:ChocolateyPackageName
-    fileFullPath  = $jarPath
-    url           = 'http://www.benf.org/other/cfr/cfr_0_133.jar'
-    checksum      = '0c4fb231158dda6d52f9edc51408c7bafd9437ee3e871715193cd86220a9f440'
-    checksumType  = 'sha256'
-}
-Get-ChocolateyWebFile @packageArgs
-
 [array]$javaCmds = Get-Command -Name java
-if ($javaCmds[0].CommandType -ne 'Application')
-{
+if ($javaCmds[0].CommandType -ne 'Application') {
     throw 'Unexpected Java location'
 }
 Install-Binfile -Name $env:ChocolateyPackageName -Path $javaCmds[0].Definition -Command "`"-jar $jarPath`""
--- automatic/cfr/update.ps1
+++ automatic/cfr/update.ps1
@@ -1,25 +1,23 @@
 $ErrorActionPreference = 'Stop'
 
-$root = 'http://www.benf.org/other/cfr/'
+$root = 'https://www.benf.org/other/cfr/'
 
 function global:au_GetLatest {
     $page = Invoke-WebRequest -Uri $root
     [array]$href = $page.Links | Select-Object -ExpandProperty href | Select-String -Pattern '^cfr_(\d+)_(\d+).jar'
     $groups = $href[0].Matches.Groups
     $version = '{0}.{1}' -f $groups[1], $groups[2]
-    @{ Version = $version; Url = $root + $href[0] }
+    @{ Version = $version; URL64 = $root + $href[0] }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase cfr }
 
 function global:au_SearchReplace {
     @{
-        'cfr.nuspec' = @{
-            '<version>[.\d]+</version>' = '<version>{0}</version>' -f $Latest.Version
-        }
-        'tools\chocolateyInstall.ps1' = @{
-            '(^\s*url\s*)=.*' = "`$1= '{0}'" -f $Latest.Url
-            '(^\s*checksum\s*)=.*' = "`$1= '{0}'" -f $Latest.Checksum64
+        'tools\VERIFICATION.txt' = @{
+            'SHA-256:.*' = 'SHA-256: {0}' -f $Latest.Checksum64
         }
     }
 }
 
-Update-Package
+Update-Package -ChecksumFor none
