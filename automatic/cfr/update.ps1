16cfb86e701f1cb6790d9706add5d07247496b48
Franklin Yu <franklinyu@hotmail.com>
2018-11-18 17:34:10 -0500

    Update URL pattern for CFR

--- automatic/cfr/update.ps1
+++ automatic/cfr/update.ps1
@@ -4,10 +4,8 @@ $root = 'https://www.benf.org/other/cfr/'
 
 function global:au_GetLatest {
     $page = Invoke-WebRequest -Uri $root
-    [array]$href = $page.Links | Select-Object -ExpandProperty href | Select-String -Pattern '^cfr_(\d+)_(\d+).jar'
-    $groups = $href[0].Matches.Groups
-    $version = '{0}.{1}' -f $groups[1], $groups[2]
-    @{ Version = $version; URL64 = $root + $href[0] }
+    [array]$href = $page.Links | Select-Object -ExpandProperty href | Select-String -Pattern '^cfr-(\d+.\d+).jar'
+    @{ Version = $href[0].Matches.Groups[1]; URL64 = $root + $href[0] }
 }
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase cfr }
