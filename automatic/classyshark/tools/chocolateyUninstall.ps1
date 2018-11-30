0e33638b19d3c529ec223b224433c60ca70159a2
Franklin Yu <franklinyu@hotmail.com>
2018-11-30 01:00:21 -0500

    Create package: ClassyShark

---
+++ automatic/classyshark/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿Uninstall-BinFile -Name $env:ChocolateyPackageName
+
+if (Test-ProcessAdminRights) {
+  $specialFolder = [Environment+SpecialFolder]::CommonPrograms
+} else {
+  $specialFolder = [Environment+SpecialFolder]::Programs
+}
+$linkPath = [Environment]::GetFolderPath($specialFolder) | Join-Path -ChildPath 'ClassyShark.lnk' | Remove-Item
