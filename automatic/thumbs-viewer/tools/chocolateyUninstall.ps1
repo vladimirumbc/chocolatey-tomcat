68faaf21bd40a34685800eae19387f12ba4c5bce
Franklin Yu <franklinyu@hotmail.com>
2019-01-06 22:14:26 -0500

    Add package Thumbs Viewer

---
+++ automatic/thumbs-viewer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+if (Test-ProcessAdminRights) {
+    $specialFolder = [Environment+SpecialFolder]::CommonPrograms
+} else {
+    $specialFolder = [Environment+SpecialFolder]::Programs
+}
+[Environment]::GetFolderPath($specialFolder) | Join-Path -ChildPath 'Thumbs Viewer.lnk' | Remove-Item
