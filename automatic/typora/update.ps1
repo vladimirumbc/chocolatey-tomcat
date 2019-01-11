32df0a2a17bfd86e448b1c1b840fdb97cec065f2
Franklin Yu <franklinyu@hotmail.com>
2019-01-10 22:31:51 -0500

    [AU typora:0.9.62] Fix 64-bit checksum

--- automatic/typora/update.ps1
+++ automatic/typora/update.ps1
@@ -6,7 +6,7 @@ function global:au_GetLatest {
     @{
         Version = $assets[0].version
         URL32 = $assets | Where-Object -Property os -EQ windows-x86 | Select-Object -ExpandProperty url
-        URL = $assets | Where-Object -Property os -EQ windows-x64 | Select-Object -ExpandProperty url
+        URL64 = $assets | Where-Object -Property os -EQ windows-x64 | Select-Object -ExpandProperty url
     }
 }
 
@@ -15,8 +15,8 @@ function global:au_SearchReplace {
         'tools\chocolateyInstall.ps1' = @{
             '(^\s*url\s*)=.*' = "`$1= '{0}'" -f $Latest.URL32
             '(^\s*checksum\s*)=.*' = "`$1= '{0}'" -f $Latest.Checksum32
-            '(^\s*url64bit\s*)=.*' = "`$1= '{0}'" -f $Latest.URL
-            '(^\s*checksum64\s*)=.*' = "`$1= '{0}'" -f $Latest.Checksum
+            '(^\s*url64bit\s*)=.*' = "`$1= '{0}'" -f $Latest.URL64
+            '(^\s*checksum64\s*)=.*' = "`$1= '{0}'" -f $Latest.Checksum64
         }
     }
 }
