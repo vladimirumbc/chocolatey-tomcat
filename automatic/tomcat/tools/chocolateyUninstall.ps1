fbdc697d8d13aec87f590f99d86038fba05b428a
Chocolatey <chocolatey@realdimensions.net>
2019-05-14 03:20:06 +0000

    AU: 1 updated - tomcat [skip ci] https://gist.github.com/7d4536b29dacffbc807aaa67914180c6/52f588ad1aff209e4b54076dcc9d98ac12b56559

--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.19"
+$zipContentFolderName = "apache-tomcat-9.0.20"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
