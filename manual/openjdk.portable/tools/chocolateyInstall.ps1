d3ed543d2caa2e6567102b25b1eab38c3d60256f
Franklin Yu <franklinyu@hotmail.com>
2019-08-20 01:47:18 -0400

    Fix OpenJDK download URL

--- manual/openjdk.portable/tools/chocolateyInstall.ps1
+++ manual/openjdk.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $pkgArgs = @{
     packageName = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
-    url64bit = "https://download.java.net/java/GA/jdk11/7/GPL/openjdk-11.0.2_windows-x64_bin.zip"
-    checksum64 = '74b13684729a249d32fd955fd1de2bec22e627f6a6a5894ca74f88c945c95f55'
+    url64bit = "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
+    checksum64 = 'cf39490fe042dba1b61d6e9a395095279a69e70086c8c8d5466d9926d80976d8'
     checksumType64 = 'sha256'
 }
 Install-ChocolateyZipPackage @pkgArgs
