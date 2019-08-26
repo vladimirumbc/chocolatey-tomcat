f538e2722f5a929617d358af05def490d2c28884
Franklin Yu <franklinyu@hotmail.com>
2019-08-26 11:18:33 -0400

    Update OpenJDK

--- manual/openjdk.portable/tools/chocolateyInstall.ps1
+++ manual/openjdk.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $pkgArgs = @{
     packageName = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
-    url64bit = "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
-    checksum64 = 'cf39490fe042dba1b61d6e9a395095279a69e70086c8c8d5466d9926d80976d8'
+    url64bit = "https://download.java.net/java/GA/jdk12/33/GPL/openjdk-12_windows-x64_bin.zip"
+    checksum64 = '35a8d018f420fb05fe7c2aa9933122896ca50bd23dbd373e90d8e2f3897c4e92'
     checksumType64 = 'sha256'
 }
 Install-ChocolateyZipPackage @pkgArgs
