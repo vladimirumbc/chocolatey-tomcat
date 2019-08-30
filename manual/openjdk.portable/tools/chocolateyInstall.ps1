a24b7bf365e304166add96b79a04936dd996889d
Franklin Yu <franklinyu@hotmail.com>
2019-08-30 11:03:03 -0400

    Update OpenJDK to 12.0.1

--- manual/openjdk.portable/tools/chocolateyInstall.ps1
+++ manual/openjdk.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $pkgArgs = @{
     packageName = $env:ChocolateyPackageName
     unzipLocation = $toolsDir
-    url64bit = "https://download.java.net/java/GA/jdk12/33/GPL/openjdk-12_windows-x64_bin.zip"
-    checksum64 = '35a8d018f420fb05fe7c2aa9933122896ca50bd23dbd373e90d8e2f3897c4e92'
+    url64bit = 'https://download.java.net/java/GA/jdk12.0.1/69cfe15208a647278a19ef0990eea691/12/GPL/openjdk-12.0.1_windows-x64_bin.zip'
+    checksum64 = 'fc7d9eee3c09ea6548b00ca25dbf34a348b3942c815405a1428e0bfef268d08d'
     checksumType64 = 'sha256'
 }
 Install-ChocolateyZipPackage @pkgArgs
@@ -17,7 +17,7 @@ if (Test-ProcessAdminRights) {
 }
 $envArgs = @{
     variableName = 'JAVA_HOME'
-    variableValue = "$toolsDir\jdk-12"
+    variableValue = "$toolsDir\jdk-12.0.1"
     variableType = $pathType
 }
 Install-ChocolateyEnvironmentVariable @envArgs
