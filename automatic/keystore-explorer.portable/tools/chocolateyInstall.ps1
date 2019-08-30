e8344d63237112820d93c7f9aed2df5e609ea756
Franklin Yu <franklinyu@hotmail.com>
2019-08-30 15:32:06 -0400

    Remove Java dependency from KeyStore Explorer

--- automatic/keystore-explorer.portable/tools/chocolateyInstall.ps1
+++ automatic/keystore-explorer.portable/tools/chocolateyInstall.ps1
@@ -3,21 +3,18 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $folder = 'kse-541'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = 'https://github.com/kaikramer/keystore-explorer/releases/download/v5.4.1/kse-541.zip'
-  checksum      = '9a112b8b71322088cd62c40a84e234497da0cecd8b4e02f68d2799355d69d35d'
-  checksumType  = 'sha256'
+    packageName = $env:ChocolateyPackageName
+    unzipLocation = $toolsDir
+    url = 'https://github.com/kaikramer/keystore-explorer/releases/download/v5.4.1/kse-541.zip'
+    checksum = '9a112b8b71322088cd62c40a84e234497da0cecd8b4e02f68d2799355d69d35d'
+    checksumType = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-if (Test-ProcessAdminRights)
-{
+if (Test-ProcessAdminRights) {
     $specialFolder = [Environment+SpecialFolder]::CommonPrograms
-}
-else
-{
+} else {
     $specialFolder = [Environment+SpecialFolder]::Programs
 }
 
