2a91da91ba4583fd1f67b6409b39e584ff917a6f
Chocolatey <chocolatey@realdimensions.net>
2019-09-20 03:20:44 +0000

    AU: 1 updated - tomcat [skip ci] https://gist.github.com/7d4536b29dacffbc807aaa67914180c6/60bfcf50bacb58c552164b85fbdca1c17a6847e9

--- automatic/tomcat/tools/chocolateyInstall.ps1
+++ automatic/tomcat/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$filename32 = "apache-tomcat-9.0.24-windows-x86.zip"
-$filename64 = "apache-tomcat-9.0.24-windows-x64.zip"
-$zipContentFolderName = "apache-tomcat-9.0.24"
+$filename32 = "apache-tomcat-9.0.26-windows-x86.zip"
+$filename64 = "apache-tomcat-9.0.26-windows-x64.zip"
+$zipContentFolderName = "apache-tomcat-9.0.26"
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.24"
+$zipContentFolderName = "apache-tomcat-9.0.26"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
