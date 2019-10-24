18aff56e519b73a640ad562c836b1c2e78035505
Chocolatey <chocolatey@realdimensions.net>
2019-10-24 03:20:58 +0000

    AU: 1 updated - typora [skip ci] https://gist.github.com/7d4536b29dacffbc807aaa67914180c6/dbf3edb6e7b30d7dd58847e313a54a9987efd0a0

--- automatic/typora/tools/chocolateyInstall.ps1
+++ automatic/typora/tools/chocolateyInstall.ps1
@@ -2,12 +2,12 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://typora.io/windows/typora-update-ia32-1011.exe'
-  url64bit       = 'https://typora.io/windows/typora-update-x64-1011.exe'
+  url            = 'https://typora.io/windows/typora-update-ia32-1022.exe'
+  url64bit       = 'https://typora.io/windows/typora-update-x64-1022.exe'
 
-  checksum       = '14b9c0077293de4b16a68a4dfb8839dfcfb7c8f926a6e6cf13cdba765bd75d57'
+  checksum       = 'd0a7a7c170ea3cec186f2c0270b11121b7184765d24da9b29ff8cf62dc79b542'
   checksumType   = 'sha256'
-  checksum64     = '87a1e56fc3a222580d2aa4e4e1a6b77d76bb26809688e97a22067b69353ddf05'
+  checksum64     = 'c899f10f6f8c09929bb4b7d318865db25daf3e4188740413cda233d64c563a04'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
