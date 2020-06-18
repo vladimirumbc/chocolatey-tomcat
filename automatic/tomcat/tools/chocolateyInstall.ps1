6563ee92da7223f660b1fe6e0f7105e69a2c5803
Thomas Flynn <tom@mybentek.com>
2020-06-18 09:43:47 -0400

    Updated tomcat version

--- automatic/tomcat/tools/chocolateyInstall.ps1
+++ automatic/tomcat/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$filename32 = "apache-tomcat-9.0.34-windows-x86.zip"
-$filename64 = "apache-tomcat-9.0.34-windows-x64.zip"
-$zipContentFolderName = "apache-tomcat-9.0.34"
+$filename32 = "apache-tomcat-9.0.36-windows-x86.zip"
+$filename64 = "apache-tomcat-9.0.36-windows-x64.zip"
+$zipContentFolderName = "apache-tomcat-9.0.36"
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
