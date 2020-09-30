4fecf5e15c9ecba4b8d111dcba3ab280454db5e5
Thomas Flynn <tom@mybentek.com>
2020-09-30 08:52:05 -0400

    Updating tomcat to latest version

--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.37"
+$zipContentFolderName = "apache-tomcat-9.0.38"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
