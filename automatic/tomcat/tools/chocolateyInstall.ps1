bff448d11ffd9c35ddb03350576cb9afbcf436ac
Thomas Flynn <thomas.flynn@mybentek.com>
2024-09-24 13:21:44 -0400

    New version of Tomcat

--- automatic/tomcat/tools/chocolateyInstall.ps1
+++ automatic/tomcat/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-$filename32 = "apache-tomcat-9.0.82-windows-x86.zip"
-$filename64 = "apache-tomcat-9.0.82-windows-x64.zip"
-$zipContentFolderName = "apache-tomcat-9.0.82"
+$filename32 = "apache-tomcat-9.0.95-windows-x86.zip"
+$filename64 = "apache-tomcat-9.0.95-windows-x64.zip"
+$zipContentFolderName = "apache-tomcat-9.0.95"
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
--- automatic/tomcat/tools/chocolateyUninstall.ps1
+++ automatic/tomcat/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$zipContentFolderName = "apache-tomcat-9.0.82"
+$zipContentFolderName = "apache-tomcat-9.0.95"
 $serviceBat = Split-Path -parent $MyInvocation.MyCommand.Definition |
     Join-Path -ChildPath $zipContentFolderName |
     Join-Path -ChildPath bin |
